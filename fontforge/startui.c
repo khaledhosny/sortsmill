e0b89bc958de154286f2b4891fbfd595248dbde0
Joe Da Silva <digital@joescat.com>
2012-09-26 14:23:51 -0700

    In main(), if argv[i]="-home", then only need to try to chdir to HOME once.

--- fontforge/startui.c
+++ fontforge/startui.c
@@ -778,8 +778,13 @@ int fontforge_main( int argc, char **argv ) {
 	    PACKAGE_STRING );
 
     /* Must be done before we cache the current directory */
-    for ( i=1; i<argc; ++i ) if ( strcmp(argv[i],"-home")==0 && getenv("HOME")!=NULL )
-	chdir(getenv("HOME"));
+    /* Change to HOME dir if specified on the commandline */
+    for ( i=1; i<argc; ++i ) {
+	if (strcmp(argv[i],"-home")==0) {
+	    if (getenv("HOME")!=NULL) chdir(getenv("HOME"));
+	    break;
+	}
+    }
 	
 #if defined(__Mac)
     /* Start X if they haven't already done so. Well... try anyway */
@@ -980,11 +985,10 @@ int fontforge_main( int argc, char **argv ) {
 	    exit(0);
 	} else if ( strcmp(pt,"-quit")==0 )
 	    quit_request = true;
-	else if ( strcmp(pt,"-home")==0 ) {
-	    if ( getenv("HOME")!=NULL )
-		chdir(getenv("HOME"));
+	else if ( strcmp(pt,"-home")==0 )
+	    /* already did a chdir earlier, don't need to do it again */;
 #if defined(__Mac)
-	} else if ( strncmp(pt,"-psn_",5)==0 ) {
+	else if ( strncmp(pt,"-psn_",5)==0 ) {
 	    /* OK, I don't know what this really means, but to me it means */
 	    /*  that we've been started on the mac from the FontForge.app  */
 	    /*  structure, and the current directory is (shudder) "/" */
@@ -992,8 +996,8 @@ int fontforge_main( int argc, char **argv ) {
 	    if ( getenv("HOME")!=NULL )
 		chdir(getenv("HOME"));
 	    listen_to_apple_events = true;
-#endif
 	}
+#endif
     }
 
     GDrawCreateDisplays(display,argv[0]);
