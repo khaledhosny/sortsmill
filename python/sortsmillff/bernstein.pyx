fdc7c5be00080ddf510e0e08d3eda028a0926a37
Barry Schwartz <github@crudfactory.com>
2012-11-09 00:26:28 -0600

    bernstein.c interface for Python, and tests for it.

--- python/sortsmillff/bernstein.pyx
+++ python/sortsmillff/bernstein.pyx
@@ -22,6 +22,12 @@ cimport numpy as np
 
 cdef extern void sbern_to_bern_double (unsigned int deg, double *sbern, double *bern)
 cdef extern void bern_to_sbern_double (unsigned int deg, double *bern, double *sbern)
+cdef extern double eval_sbern_double (unsigned int deg, double *spline, double t)
+cdef extern double eval_bern_double (unsigned int deg, double *spline, double t)
+cdef extern double evaldc_sbern_double (unsigned int deg, double *spline, double t)
+cdef extern double evaldc_bern_double (unsigned int deg, double *spline, double t)
+cdef extern void subdiv_sbern_double (unsigned int deg, double *spline, double t, double *a, double *b)
+cdef extern void subdiv_bern_double (unsigned int deg, double *spline, double t, double *a, double *b)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -41,3 +47,48 @@ def bern_to_sbern (np.ndarray[double, ndim=1, mode="c"] spline not None):
     bern_to_sbern_double (deg, &spline[0], &cresult[0])
     return result
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def eval_sbern (np.ndarray[double, ndim=1, mode="c"] spline not None, double t):
+    cdef int deg = spline.shape[0] - 1
+    return eval_sbern_double (deg, &spline[0], t)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def eval_bern (np.ndarray[double, ndim=1, mode="c"] spline not None, double t):
+    cdef int deg = spline.shape[0] - 1
+    return eval_bern_double (deg, &spline[0], t)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def evaldc_sbern (np.ndarray[double, ndim=1, mode="c"] spline not None, double t):
+    cdef int deg = spline.shape[0] - 1
+    return evaldc_sbern_double (deg, &spline[0], t)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def evaldc_bern (np.ndarray[double, ndim=1, mode="c"] spline not None, double t):
+    cdef int deg = spline.shape[0] - 1
+    return evaldc_bern_double (deg, &spline[0], t)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def subdiv_sbern (np.ndarray[double, ndim=1, mode="c"] spline not None, double t):
+    cdef int deg = spline.shape[0] - 1
+    a = np.empty_like (spline)
+    cdef np.ndarray[double, ndim=1, mode="c"] ca = a
+    b = np.empty_like (spline)
+    cdef np.ndarray[double, ndim=1, mode="c"] cb = b
+    subdiv_sbern_double (deg, &spline[0], t, &ca[0], &cb[0])
+    return (a, b)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def subdiv_bern (np.ndarray[double, ndim=1, mode="c"] spline not None, double t):
+    cdef int deg = spline.shape[0] - 1
+    a = np.empty_like (spline)
+    cdef np.ndarray[double, ndim=1, mode="c"] ca = a
+    b = np.empty_like (spline)
+    cdef np.ndarray[double, ndim=1, mode="c"] cb = b
+    subdiv_bern_double (deg, &spline[0], t, &ca[0], &cb[0])
+    return (a, b)
---
+++ tests/test_python_bernstein.py
@@ -0,0 +1,72 @@
+#! /bin/env python
+#-*- coding:utf-8; python-indent: 2; -*-
+
+import numpy as np
+import sortsmillff.bernstein as bern
+import sys
+
+epsilon = sys.float_info.epsilon
+write = sys.stdout.write
+
+spline1 = [5, 4, -3, 2, 1, 0, 1, -2, 3, 4, 5]
+times = [0, 0.25, 0.5, 0.75, 1]
+
+write ("eval_sbern_double\n")
+for deg in range (0, 11):
+  for t in times:
+    b = np.array (spline1[:deg + 1], dtype=np.double)
+    write ("{:.6f}|".format (bern.eval_sbern (b, t)))
+  write ("\n")
+
+write ("eval_bern_double\n")
+for deg in range (0, 11):
+  for t in times:
+    b = np.array (spline1[:deg + 1], dtype=np.double)
+    write ("{:.6f}|".format (bern.eval_bern (b, t)))
+  write ("\n")
+
+write ("evaldc_sbern_double\n")
+for deg in range (0, 11):
+  for t in times:
+    b = np.array (spline1[:deg + 1], dtype=np.double)
+    write ("{:.6f}|".format (bern.evaldc_sbern (b, t)))
+  write ("\n")
+
+write ("evaldc_bern_double\n")
+for deg in range (0, 11):
+  for t in times:
+    b = np.array (spline1[:deg + 1], dtype=np.double)
+    write ("{:.6f}|".format (bern.evaldc_bern (b, t)))
+  write ("\n")
+
+write ("subdiv_sbern_double\n")
+for deg in range (0, 5):
+  for t in times:
+    write ("t={:.6f}|".format (t))
+    s = np.array (spline1[:deg + 1], dtype=np.double)
+    (a, b) = bern.subdiv_sbern (s, t)
+    for j in range (0, deg + 1):
+      write ("{:.6f}|".format (a[j]))
+    for j in range (0, deg + 1):
+      write ("{:.6f}|".format (b[j]))
+    write ("\n")
+
+write ("subdiv_bern_double\n")
+for deg in range (0, 5):
+  for t in times:
+    write ("t={:.6f}|".format (t))
+    s = np.array (spline1[:deg + 1], dtype=np.double)
+    (a, b) = bern.subdiv_bern (s, t)
+    for j in range (0, deg + 1):
+      write ("{:.6f}|".format (a[j]))
+    for j in range (0, deg + 1):
+      write ("{:.6f}|".format (b[j]))
+
+    # Check that subdivision gives the same result as
+    # evaluation.
+    v = bern.eval_bern (s, t)
+    difference = abs (b[0] - v)
+    close_enough = (difference <= 10 * epsilon)
+    write ("{:1d}".format (close_enough))
+
+    write ("\n")
