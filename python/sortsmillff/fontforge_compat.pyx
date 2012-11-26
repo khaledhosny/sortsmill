86a6027316cc231241915550eb5e2056dc5d02a9
Barry Schwartz <github@crudfactory.com>
2012-11-26 16:56:59 -0600

    Checking in work so far on Python compat module before converting it to regular Python.

--- python/sortsmillff/fontforge_compat.pyx
+++ python/sortsmillff/fontforge_compat.pyx
@@ -16,6 +16,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 cimport contour_interface as ci
+from cpython cimport ref
 from cython cimport view
 from cython.view cimport array as cvarray
 from libcpp cimport bool
@@ -104,8 +105,19 @@ cdef class point (object):
   selected = property (__get_selected, __set_selected)
   name = property (__get_name, __set_name)
 
+  def __richcmp__ (self, other, op):
+    if not isinstance (self, point) or not isinstance (other, point):
+      result = False
+    elif op == 2:               # ==
+      result = (self.x == other.x and self.y == other.y)
+    elif op == 3:               # !=
+      result = (self.x != other.x or self.y != other.y)
+    else:
+      result = False
+    return result
+
   def __repr__ (self):
-    return ('{}.point(x={:g},y={:g},on_curve={},selected={},name={})'
+    return ('{}.point(x={},y={},on_curve={},selected={},name={})'
             .format (__name__, self.__x, self.__y,
                      repr (self.__on_curve),
                      repr (self.__selected),
@@ -122,24 +134,28 @@ cdef class point (object):
 
 #--------------------------------------------------------------------------
 #
-# FIXME: Some operations on contours in regular FontForge are either
-# broken outright or have unusual semantics. Our current attitude is
-# that these are all bugs, and programs depending on the behaviors
-# therefore are broken. Our ‘contour’ class is a subclass of list and
-# inherits the semantics of lists.
+# FIXME: List-like operations on contours in regular FontForge are
+# either broken outright or seem to have unusual semantics. Our
+# current attitude is that these behaviors all are BUGS; programs
+# depending on those behaviors therefore are BROKEN and unsupported by
+# us.
+#
+# Our ‘contour’ class simply borrows the semantics of lists.
 
-cdef class contour (list):
+cdef class contour (object):
 
+  cdef object __points
   cdef bool __closed
   cdef bool __is_quadratic
   cdef object __name
 
   def __init__ (self, points = [], closed = False, is_quadratic = False, name = None):
-    super (contour, self).__init__ (points)
+
+    self.__points = list (points)
     self.__closed = closed
     self.__is_quadratic = is_quadratic
     self.name = name
-    
+
   def __get_closed (self):
     return self.__closed
 
@@ -185,12 +201,92 @@ cdef class contour (list):
     s += '>'
     return s
 
+  def __len__ (self):
+    return len (self.__points)
+
+  def __getitem__ (self, key):
+    result = self.__points[key]
+    if not isinstance (result, point):
+      result = contour (result, closed = False,
+                        is_quadratic = self.__is_quadratic,
+                        name = None)
+    return result
+
+  def __setitem__ (self, key, item):
+    if isinstance (item, point):
+      self.__points[key] = item
+    elif all ((isinstance (it, point) for it in item)):
+      self.__points[key] = list (item)
+    else:
+      raise TypeError ('expected a point or points')
+
+  def __delitem__ (self, key):
+    self.__points.__delitem__ (key)
+
+  def __contains__ (self, item):
+    return (item in self.__points)
+
+  def __iter__ (self):
+    return self.__points.__iter__ ()
+
+  def __add__ (self, other):
+    if isinstance (self, contour):
+      result = self.dup ()
+      result.__iadd__ (other)
+    else:
+      result = NotImplemented
+    return result
+
+  def __iadd__ (self, other):
+    if isinstance (other, point):
+      self.__points.append (other)
+    elif all ((isinstance (p, point) for p in other)):
+      # An extension over regular FontForge: accept general
+      # iterables of points.
+      if isinstance (other, contour) and self.is_quadratic != other.is_quadratic:
+        raise ValueError ('mixed contour degrees')
+      self.__points += list (other)
+    else:
+      raise TypeError ('expected a point or points')
+
   def dup (self):
-    return contour ([p.dup () for p in self], closed = self.__closed,
-                    is_quadratic = self.__is_quadratic, name = self.__name)
+    return contour ([p.dup () for p in self.__points],
+                    closed = self.__closed,
+                    is_quadratic = self.__is_quadratic,
+                    name = self.__name)
 
   def isEmpty (self):
-    return (len (self) == 0)
+    return (len (self.__points) == 0)
+
+  def moveTo (self, x, y, bool selected = False, name = None):
+    if len (self.__points) != 0:
+      raise ValueError ('contour not empty')
+    self += point (x, y, on_curve = True, selected = selected, name = name)
+    ref.Py_INCREF (self)
+    return self
+
+  def lineTo (self, double x, double y, pos = None,
+              bool selected = False, name = None):
+    cdef size_t i = self.__find_on_curve_point (pos)
+    self.__points = (self.__points[:i + 1] +
+                     [point (x, y, selected = selected, name = name)] +
+                     self.__points[i + 1:])
+    ref.Py_INCREF (self)
+    return self
+
+  def __find_on_curve_point (self, pos, bool check_empty = True):
+    if check_empty and len (self.__points) == 0:
+      raise ValueError ('contour empty')
+    if pos is None:
+      pos = len (self.__points) - 1
+    if pos < 0 or len (self.__points) <= pos:
+      raise IndexError ('expected 0 <= index < ' + str (len (self.__points)))
+    cdef size_t i = pos
+    while 0 < i and not self.__points[i].on_curve:
+      i -= 1
+    if not self.__points[i].on_curve:
+      raise ValueError ('no on-curve points before position ' + str (pos))
+    return i
 
 #--------------------------------------------------------------------------
 
