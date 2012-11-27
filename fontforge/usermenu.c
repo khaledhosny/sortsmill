7e8800433884afbba663583957314c0beac1cc48
Barry Schwartz <github@crudfactory.com>
2012-11-27 02:22:20 -0600

    Move user-interface parts of fontforge/usermenu.h to sortsmillff/usermenu.h

--- fontforge/usermenu.c
+++ fontforge/usermenu.c
@@ -31,10 +31,11 @@
 #include "usermenu.h"
 #include "ustring.h"
 
-typedef struct menu_info {
-    menu_info_func func;
-    menu_info_check check_enabled;
-    menu_info_data data;
+typedef struct menu_info
+{
+  menu_info_func func;
+  menu_info_check check_enabled;
+  menu_info_data data;
 } MenuInfo;
 
 static MenuInfo *cv_menu_data = NULL;
@@ -48,202 +49,224 @@ GMenuItem2 *cv_menu;
 GMenuItem2 *fv_menu;
 
 static void
-tl2listcheck(struct gmenuitem *mi,
-             void *owner,
-             struct menu_info *menu_data,
-             int menu_cnt)
+tl2listcheck (struct gmenuitem *mi,
+              void *owner, struct menu_info *menu_data, int menu_cnt)
 {
-    int result;
+  int result;
 
-    if (menu_data==NULL)
-        return;
+  if (menu_data == NULL)
+    return;
 
-    for (mi = mi->sub; mi->ti.text != NULL || mi->ti.line; ++mi) {
-        if (mi->mid == -1)		/* Submenu */
-            continue;
-        if (mi->mid < 0 || mi->mid >= menu_cnt) {
-            fprintf(stderr, "Bad Menu ID in python menu %d\n", mi->mid);
-            mi->ti.disabled = true;
-            continue;
+  for (mi = mi->sub; mi->ti.text != NULL || mi->ti.line; ++mi)
+    {
+      if (mi->mid == -1)        /* Submenu */
+        continue;
+      if (mi->mid < 0 || mi->mid >= menu_cnt)
+        {
+          fprintf (stderr, "Bad Menu ID in python menu %d\n", mi->mid);
+          mi->ti.disabled = true;
+          continue;
         }
-        if (menu_data[mi->mid].check_enabled == NULL) {
-            mi->ti.disabled = false;
-            continue;
+      if (menu_data[mi->mid].check_enabled == NULL)
+        {
+          mi->ti.disabled = false;
+          continue;
         }
-        result = (*menu_data[mi->mid].check_enabled)(menu_data[mi->mid].data, owner);
-        mi->ti.disabled = (result == 0);
+      result =
+        (*menu_data[mi->mid].check_enabled) (menu_data[mi->mid].data, owner);
+      mi->ti.disabled = (result == 0);
     }
 }
 
 void
-cv_tl2listcheck(GWindow gw, struct gmenuitem *mi, GEvent *e)
+cv_tl2listcheck (GWindow gw, struct gmenuitem *mi, GEvent *e)
 {
-    CharView *cv = (CharView *) GDrawGetUserData(gw);
+  CharView *cv = (CharView *) GDrawGetUserData (gw);
 
-    if (cv_menu_data != NULL) {
-        sc_active_in_ui = cv->b.sc;
-        layer_active_in_ui = CVLayer((CharViewBase *) cv);
-        tl2listcheck(mi, cv->b.sc, cv_menu_data, cv_menu_cnt);
-        sc_active_in_ui = NULL;
-        layer_active_in_ui = ly_fore;
+  if (cv_menu_data != NULL)
+    {
+      sc_active_in_ui = cv->b.sc;
+      layer_active_in_ui = CVLayer ((CharViewBase *) cv);
+      tl2listcheck (mi, cv->b.sc, cv_menu_data, cv_menu_cnt);
+      sc_active_in_ui = NULL;
+      layer_active_in_ui = ly_fore;
     }
 }
 
 void
-fv_tl2listcheck(GWindow gw, struct gmenuitem *mi, GEvent *e)
+fv_tl2listcheck (GWindow gw, struct gmenuitem *mi, GEvent *e)
 {
-    FontViewBase *fv = (FontViewBase *) GDrawGetUserData(gw);
+  FontViewBase *fv = (FontViewBase *) GDrawGetUserData (gw);
 
-    if (fv_menu_data != NULL) { 
-        fv_active_in_ui = fv;
-        layer_active_in_ui = fv->active_layer;
-        tl2listcheck(mi, fv, fv_menu_data, fv_menu_cnt);
-        fv_active_in_ui = NULL;
+  if (fv_menu_data != NULL)
+    {
+      fv_active_in_ui = fv;
+      layer_active_in_ui = fv->active_layer;
+      tl2listcheck (mi, fv, fv_menu_data, fv_menu_cnt);
+      fv_active_in_ui = NULL;
     }
 }
 
 static void
-menuactivate(struct gmenuitem *mi,
-             void *owner,
-             MenuInfo *menu_data,
-             int menu_cnt)
+menuactivate (struct gmenuitem *mi,
+              void *owner, MenuInfo * menu_data, int menu_cnt)
 {
-    if (mi->mid == -1)          /* Submenu */
-        return;
-    if (mi->mid < 0 || mi->mid >= menu_cnt) {
-        fprintf(stderr, "Bad Menu ID in python menu %d\n", mi->mid);
-        return;
+  if (mi->mid == -1)            /* Submenu */
+    return;
+  if (mi->mid < 0 || mi->mid >= menu_cnt)
+    {
+      fprintf (stderr, "Bad Menu ID in python menu %d\n", mi->mid);
+      return;
     }
-    if (menu_data[mi->mid].func == NULL) {
-        return;
+  if (menu_data[mi->mid].func == NULL)
+    {
+      return;
     }
-    (*menu_data[mi->mid].func)(menu_data[mi->mid].data, owner);
+  (*menu_data[mi->mid].func) (menu_data[mi->mid].data, owner);
 }
 
 static void
-cv_menuactivate(GWindow gw, struct gmenuitem *mi, GEvent *e)
+cv_menuactivate (GWindow gw, struct gmenuitem *mi, GEvent *e)
 {
-    CharView *cv = (CharView *) GDrawGetUserData(gw);
+  CharView *cv = (CharView *) GDrawGetUserData (gw);
 
-    if (cv_menu_data != NULL) {
-        sc_active_in_ui = cv->b.sc;
-        layer_active_in_ui = CVLayer((CharViewBase *) cv);
-        menuactivate(mi, cv->b.sc, cv_menu_data, cv_menu_cnt);
-        sc_active_in_ui = NULL;
-        layer_active_in_ui = ly_fore;
+  if (cv_menu_data != NULL)
+    {
+      sc_active_in_ui = cv->b.sc;
+      layer_active_in_ui = CVLayer ((CharViewBase *) cv);
+      menuactivate (mi, cv->b.sc, cv_menu_data, cv_menu_cnt);
+      sc_active_in_ui = NULL;
+      layer_active_in_ui = ly_fore;
     }
 }
 
 static void
-fv_menuactivate(GWindow gw, struct gmenuitem *mi, GEvent *e)
+fv_menuactivate (GWindow gw, struct gmenuitem *mi, GEvent *e)
 {
-    FontViewBase *fv = (FontViewBase *) GDrawGetUserData(gw);
+  FontViewBase *fv = (FontViewBase *) GDrawGetUserData (gw);
 
-    if (fv_menu_data!=NULL) {
-        fv_active_in_ui = fv;
-        layer_active_in_ui = fv->active_layer;
-        menuactivate(mi, fv, fv_menu_data, fv_menu_cnt);
-        fv_active_in_ui = NULL;
+  if (fv_menu_data != NULL)
+    {
+      fv_active_in_ui = fv;
+      layer_active_in_ui = fv->active_layer;
+      menuactivate (mi, fv, fv_menu_data, fv_menu_cnt);
+      fv_active_in_ui = NULL;
     }
 }
 
 static int
-MenuDataAdd(menu_info_func func, menu_info_check check, menu_info_data data, int is_cv)
+MenuDataAdd (menu_info_func func, menu_info_check check, menu_info_data data,
+             int is_cv)
 {
-    int index;
+  int index;
 
-    if (is_cv) {
-        if (cv_menu_cnt >= cv_menu_max)
-            cv_menu_data = xrealloc(cv_menu_data,(cv_menu_max+=10)*sizeof(struct menu_info));
-        cv_menu_data[cv_menu_cnt].func = func;
-        cv_menu_data[cv_menu_cnt].check_enabled = check;
-        cv_menu_data[cv_menu_cnt].data = data;
-        index = cv_menu_cnt;
-        cv_menu_cnt++;
-    } else {
-        if (fv_menu_cnt >= fv_menu_max)
-            fv_menu_data = xrealloc(fv_menu_data,(fv_menu_max+=10)*sizeof(struct menu_info));
-        fv_menu_data[fv_menu_cnt].func = func;
-        fv_menu_data[fv_menu_cnt].check_enabled = check;
-        fv_menu_data[fv_menu_cnt].data = data;
-        index = fv_menu_cnt;
-        fv_menu_cnt++;
+  if (is_cv)
+    {
+      if (cv_menu_cnt >= cv_menu_max)
+        cv_menu_data =
+          xrealloc (cv_menu_data,
+                    (cv_menu_max += 10) * sizeof (struct menu_info));
+      cv_menu_data[cv_menu_cnt].func = func;
+      cv_menu_data[cv_menu_cnt].check_enabled = check;
+      cv_menu_data[cv_menu_cnt].data = data;
+      index = cv_menu_cnt;
+      cv_menu_cnt++;
     }
-    return index;
+  else
+    {
+      if (fv_menu_cnt >= fv_menu_max)
+        fv_menu_data =
+          xrealloc (fv_menu_data,
+                    (fv_menu_max += 10) * sizeof (struct menu_info));
+      fv_menu_data[fv_menu_cnt].func = func;
+      fv_menu_data[fv_menu_cnt].check_enabled = check;
+      fv_menu_data[fv_menu_cnt].data = data;
+      index = fv_menu_cnt;
+      fv_menu_cnt++;
+    }
+  return index;
 }
 
 static void
-InsertSubMenus(menu_info_func func,
-               menu_info_check check,
-               menu_info_data data,
-               char *shortcut_str,
-               char **submenu_names,
-               GMenuItem2 **mn,
-               int is_cv)
+InsertSubMenus (menu_info_func func,
+                menu_info_check check,
+                menu_info_data data,
+                char *shortcut_str,
+                char **submenu_names, GMenuItem2 ** mn, int is_cv)
 {
-    int i;
-    int j;
-    GMenuItem2 *mmn;
+  int i;
+  int j;
+  GMenuItem2 *mmn;
 
-    for (i = 0; submenu_names[i] != NULL; ++i) {
-        uint32_t *submenuu = utf82u_copy(submenu_names[i]);
+  for (i = 0; submenu_names[i] != NULL; ++i)
+    {
+      uint32_t *submenuu = utf82u_copy (submenu_names[i]);
 
-        j = 0;
-        if (*mn != NULL) {
-            for (j = 0; (*mn)[j].ti.text != NULL || (*mn)[j].ti.line; ++j) {
-                if ((*mn)[j].ti.text == NULL)
-                    continue;
-		// FIXME: Should this be a normalized comparison?
-                if (u32_strcmp((*mn)[j].ti.text, submenuu) == 0)
-                    break;
+      j = 0;
+      if (*mn != NULL)
+        {
+          for (j = 0; (*mn)[j].ti.text != NULL || (*mn)[j].ti.line; ++j)
+            {
+              if ((*mn)[j].ti.text == NULL)
+                continue;
+              // FIXME: Should this be a normalized comparison?
+              if (u32_strcmp ((*mn)[j].ti.text, submenuu) == 0)
+                break;
             }
         }
-        
-        if (*mn == NULL || (*mn)[j].ti.text == NULL) {
-            *mn = xrealloc(*mn,(j+2)*sizeof(GMenuItem2));
-            memset(*mn+j,0,2*sizeof(GMenuItem2));
+
+      if (*mn == NULL || (*mn)[j].ti.text == NULL)
+        {
+          *mn = xrealloc (*mn, (j + 2) * sizeof (GMenuItem2));
+          memset (*mn + j, 0, 2 * sizeof (GMenuItem2));
         }
-        mmn = *mn;
-        if (mmn[j].ti.text == NULL) {
-            mmn[j].ti.text = submenuu;
-            mmn[j].ti.fg = mmn[j].ti.bg = COLOR_DEFAULT;
-            if (submenu_names[i + 1] != NULL) {
-                mmn[j].mid = -1;
-                mmn[j].moveto = is_cv ? cv_tl2listcheck : fv_tl2listcheck;
-                mn = &mmn[j].sub;
-            } else {
-                mmn[j].shortcut = xstrdup_or_null(shortcut_str);
-                mmn[j].invoke = is_cv ? cv_menuactivate : fv_menuactivate;
-                mmn[j].mid = MenuDataAdd(func, check, data, is_cv);
+      mmn = *mn;
+      if (mmn[j].ti.text == NULL)
+        {
+          mmn[j].ti.text = submenuu;
+          mmn[j].ti.fg = mmn[j].ti.bg = COLOR_DEFAULT;
+          if (submenu_names[i + 1] != NULL)
+            {
+              mmn[j].mid = -1;
+              mmn[j].moveto = is_cv ? cv_tl2listcheck : fv_tl2listcheck;
+              mn = &mmn[j].sub;
             }
-        } else {
-            if (submenu_names[i + 1] != NULL)
-                mn = &mmn[j].sub;
-            else {
-                mmn[j].shortcut = xstrdup_or_null(shortcut_str);
-                mmn[j].invoke = is_cv ? cv_menuactivate : fv_menuactivate;
-                mmn[j].mid = MenuDataAdd(func, check, data, is_cv);
-                fprintf(stderr, "Redefining menu item %s\n", submenu_names[i]);
-                free(submenuu);
+          else
+            {
+              mmn[j].shortcut = xstrdup_or_null (shortcut_str);
+              mmn[j].invoke = is_cv ? cv_menuactivate : fv_menuactivate;
+              mmn[j].mid = MenuDataAdd (func, check, data, is_cv);
+            }
+        }
+      else
+        {
+          if (submenu_names[i + 1] != NULL)
+            mn = &mmn[j].sub;
+          else
+            {
+              mmn[j].shortcut = xstrdup_or_null (shortcut_str);
+              mmn[j].invoke = is_cv ? cv_menuactivate : fv_menuactivate;
+              mmn[j].mid = MenuDataAdd (func, check, data, is_cv);
+              fprintf (stderr, "Redefining menu item %s\n", submenu_names[i]);
+              free (submenuu);
             }
         }
     }
 }
 
 VISIBLE void
-RegisterMenuItem(menu_info_func func,
-                 menu_info_check check,
-                 menu_info_data data,
-                 int flags,
-                 char *shortcut_str,
-                 char **submenu_names)
+RegisterMenuItem (menu_info_func func,
+                  menu_info_check check,
+                  menu_info_data data,
+                  int flags, char *shortcut_str, char **submenu_names)
 {
-    if (!no_windowing_ui) {
-        if (flags & menu_fv)
-            InsertSubMenus(func, check, data, shortcut_str,
-                           submenu_names, &fv_menu, false);
-        if (flags & menu_cv)
-            InsertSubMenus(func, check, data, shortcut_str,
-                           submenu_names, &cv_menu, true);
+  if (!no_windowing_ui)
+    {
+      if (flags & menu_fv)
+        InsertSubMenus (func, check, data, shortcut_str,
+                        submenu_names, &fv_menu, false);
+      if (flags & menu_cv)
+        InsertSubMenus (func, check, data, shortcut_str,
+                        submenu_names, &cv_menu, true);
     }
 }
---
+++ inc/sortsmillff/usermenu.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2012 Barry Schwartz
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SORTSMILLFF_USERMENU_H
+#define _SORTSMILLFF_USERMENU_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#if 0
+}
+#endif
+
+enum
+{
+  menu_fv = 0x01,
+  menu_cv = 0x02
+};
+
+typedef void (*menu_info_func) (void *, void *);
+typedef int (*menu_info_check) (void *, void *);
+typedef void *menu_info_data;
+
+void RegisterMenuItem (menu_info_func func, menu_info_check check,
+                       menu_info_data data, int flags,
+                       char *shortcut_str, char **submenu_names);
+
+#if 0
+{
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SORTSMILLFF_USERMENU_H */
