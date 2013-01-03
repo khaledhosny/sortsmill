fdb5eb650607ababc0929fa56dbe4e05868ff9b6
Barry Schwartz <github@crudfactory.com>
2013-01-02 21:58:42 -0600

    Revise the README for menu-work, and extract .invoke fields so they can be made symbols.

---
+++ menu-work/extract-invoke.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export LC_ALL
+LC_ALL=C
+
+for i in *.menu_fields; do
+    sed -e '/.invoke/p;d' $i \
+        | sed -e 's/^.*=[ \t]\([A-Z_a-z][A-Z_a-z0-9]*\).*/\1/' \
+        | sort -u \
+        | sed -e '/^NULL$/d' \
+        > `basename ${i} .menu_fields`.invoke
+done
