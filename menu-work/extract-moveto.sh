6d3e1d068754783f81c0b60076891450dca54582
Barry Schwartz <github@crudfactory.com>
2013-01-02 22:40:43 -0600

    Now extract .sub and .moveto fields, which also need to be symbols.

--- menu-work/extract-moveto.sh
+++ menu-work/extract-moveto.sh
@@ -4,9 +4,9 @@ export LC_ALL
 LC_ALL=C
 
 for i in *.menu_fields; do
-    sed -e '/\.invoke[ \t]*=/p;d' $i \
+    sed -e '/\.moveto[ \t]*=/p;d' $i \
         | sed -e 's/^.*=[ \t]\([A-Z_a-z][A-Z_a-z0-9]*\).*/\1/' \
         | sort -u \
         | sed -e '/^NULL$/d' \
-        > `basename ${i} .menu_fields`.invoke
+        > `basename ${i} .menu_fields`.moveto
 done
