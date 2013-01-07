2657ac1a5700e6d4d59ded83b87049c9828f3650
Barry Schwartz <github@crudfactory.com>
2013-01-06 21:38:23 -0600

    Reformat views.scm

--- guile/sortsmillff/views.scm
+++ guile/sortsmillff/views.scm
@@ -1,4 +1,4 @@
-;; -*- mode: bee; coding: utf-8 -*-
+;; -*- mode: scheme; coding: utf-8 -*-
 
 ;; Copyright (C) 2012 Barry Schwartz
 ;; 
@@ -17,65 +17,63 @@
 
 (define-module (sortsmillff views))
 
-(use-modules
-   (sortsmillff fontforge-api)
-   (system foreign)
-   (ice-9 format)
-   )
+(use-modules (sortsmillff fontforge-api)
+             (system foreign)
+             (ice-9 format)
+             )
 
-(export
-   font-view
-   font-view?
-   pointer->font-view
-   font-view->pointer
-   glyph-view
-   glyph-view?
-   pointer->glyph-view
-   glyph-view->pointer
-   view->pointer
-   font-view->FontViewBase
-   FontViewBase->font-view
-   glyph-view->CharViewBase
-   CharViewBase->glyph-view
-   )
+(export font-view
+        font-view?
+        pointer->font-view
+        font-view->pointer
+        glyph-view
+        glyph-view?
+        pointer->glyph-view
+        glyph-view->pointer
+        view->pointer
+        font-view->FontViewBase
+        FontViewBase->font-view
+        glyph-view->CharViewBase
+        CharViewBase->glyph-view
+        )
 
 (define-wrapped-pointer-type font-view
-   font-view?
-   pointer->font-view font-view->pointer
-   (lambda (fv port)
-      (let* ((fvb (font-view->FontViewBase fv))
-             (sf (FontViewBase:sf-dref fvb)))
-         (format port "#<font-view ~s 0x~x>"
-            (pointer->string (SplineFont:font-name-ref sf))
-            (pointer-address (font-view->pointer fv))))))
+  font-view?
+  pointer->font-view font-view->pointer
+  (lambda (fv port)
+    (let* ((fvb (font-view->FontViewBase fv))
+           (sf (FontViewBase:sf-dref fvb)))
+      (format port "#<font-view ~s 0x~x>"
+              (pointer->string (SplineFont:font-name-ref sf))
+              (pointer-address (font-view->pointer fv))))))
 
 (define-wrapped-pointer-type glyph-view
-   glyph-view?
-   pointer->glyph-view glyph-view->pointer
-   (lambda (gv port)
-      (let* ((cvb (glyph-view->CharViewBase gv))
-             (sc (CharViewBase:sc-dref cvb))
-             (sf (SplineChar:parent-dref sc)))
-         (format port "#<glyph-view ~s:~s 0x~x>"
-            (pointer->string (SplineFont:font-name-ref sf))
-            (pointer->string (SplineChar:name-ref sc))
-            (pointer-address (glyph-view->pointer gv))))))
+  glyph-view?
+  pointer->glyph-view glyph-view->pointer
+  (lambda (gv port)
+    (let* ((cvb (glyph-view->CharViewBase gv))
+           (sc (CharViewBase:sc-dref cvb))
+           (sf (SplineChar:parent-dref sc)))
+      (format port "#<glyph-view ~s:~s 0x~x>"
+              (pointer->string (SplineFont:font-name-ref sf))
+              (pointer->string (SplineChar:name-ref sc))
+              (pointer-address (glyph-view->pointer gv))))))
 
 (define (view->pointer v)
-   (cond ((font-view? v) (font-view->pointer v))
-         ((glyph-view? v) (glyph-view->pointer v))
-         (else (scm-error 'wrong-type-arg "view->pointer"
-                  "Not a font-view or glyph-view: ~S"
-                  (list v) (list v)))))
+  (cond ((font-view? v) (font-view->pointer v))
+        ((glyph-view? v) (glyph-view->pointer v))
+        (else (scm-error 'wrong-type-arg "view->pointer"
+                         "Not a font-view or glyph-view: ~S"
+                         (list v) (list v)))))
 
 (define (font-view->FontViewBase fv)
-   (pointer->FontViewBase (font-view->pointer fv)))
+  (pointer->FontViewBase (font-view->pointer fv)))
 
 (define (FontViewBase->font-view fvb)
-   (pointer->font-view (FontViewBase->pointer fvb)))
+  (pointer->font-view (FontViewBase->pointer fvb)))
 
 (define (glyph-view->CharViewBase gv)
-   (pointer->CharViewBase (glyph-view->pointer gv)))
+  (pointer->CharViewBase (glyph-view->pointer gv)))
 
 (define (CharViewBase->glyph-view sc)
-   (pointer->glyph-view (CharViewBase->pointer sc)))
+  (pointer->glyph-view (CharViewBase->pointer sc)))
