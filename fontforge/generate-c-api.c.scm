114cd0247ae4463e192999a519ed6f590d7e9523
Barry Schwartz <github@crudfactory.com>
2013-01-06 21:53:31 -0600

    Reformat generate-c-api.[ch].scm and use ‘indent’ instead of ‘use-modules’.

--- fontforge/generate-c-api.c.scm
+++ fontforge/generate-c-api.c.scm
@@ -1,4 +1,4 @@
-#! @GUILE@ \           -*- mode: bee; coding: utf-8 -*-
+#! @GUILE@ \           -*- mode: scheme; coding: utf-8 -*-
 --no-auto-compile -s
 !#
 
@@ -17,78 +17,77 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-(use-modules
-   (ff-internal generate-types)
-   (ice-9 match)
-   (ice-9 format)
-   )
+(import (ff-internal generate-types)
+        (ice-9 match)
+        (ice-9 format)
+        )
 
 (define (write-instruction instruction)
-   (match instruction
-      (('struct (? string? struct-name) (? integer? size))
-       (format #t "VISIBLE void *malloc_ff_~a (void);\n" struct-name)
-       (format #t "VISIBLE void free_ff_~a (void *);\n" struct-name)
-       (format #t "VISIBLE void *gc_malloc_ff_~a (void);\n" struct-name)
-       (format #t "VISIBLE void gc_free_ff_~a (void *);\n" struct-name)
-       (format #t "\n")
-       )
-      (('sizeof (? string? struct-name) (? integer? size))
-       (format #t "VISIBLE size_t sizeof_ff_~a (void);\n" struct-name)
-       (format #t "\n")
-       )
-      (('field (and (or 'struct 'array) field-type) (? string? struct-name)
-          (? string? field-name) (? integer? offset) (? integer? size))
-       (format #t "VISIBLE void *ptr_ff_~a_~a (void *);\n"
-          struct-name field-name)
-       (format #t "\n")       
-       )
-      (('field (? symbol? field-type) (? string? struct-name)
-          (? string? field-name) (? integer? offset) (? integer? size))
-       (format #t "VISIBLE ~a get_ff_~a_~a (void *);\n"
-          (value-c-type field-type size) struct-name field-name)
-       (format #t "VISIBLE void set_ff_~a_~a (void *, ~a);\n"
-          struct-name field-name (value-c-type field-type size))
-       (format #t "VISIBLE void *ptr_ff_~a_~a (void *);\n"
-          struct-name field-name)
-       (format #t "\n")
-       )
-      (('field ((and (or '* 'struct 'array ) field-type) (? symbol? pointer-type))
-          (? string? struct-name) (? string? field-name) (? integer? offset)
-          (? integer? size))
-       (write-instruction (list 'field field-type struct-name field-name offset size))
-       ;;
-       ;; FIXME: Dereferencing and array procedures go here.
-       ;;
-       )
-      
-      ((instruction-symbol . _)
-       (format (current-error-port) "Ignoring '~a\n" instruction-symbol))
-      ))
+  (match instruction
+         (('struct (? string? struct-name) (? integer? size))
+          (format #t "VISIBLE void *malloc_ff_~a (void);\n" struct-name)
+          (format #t "VISIBLE void free_ff_~a (void *);\n" struct-name)
+          (format #t "VISIBLE void *gc_malloc_ff_~a (void);\n" struct-name)
+          (format #t "VISIBLE void gc_free_ff_~a (void *);\n" struct-name)
+          (format #t "\n")
+          )
+         (('sizeof (? string? struct-name) (? integer? size))
+          (format #t "VISIBLE size_t sizeof_ff_~a (void);\n" struct-name)
+          (format #t "\n")
+          )
+         (('field (and (or 'struct 'array) field-type) (? string? struct-name)
+                  (? string? field-name) (? integer? offset) (? integer? size))
+          (format #t "VISIBLE void *ptr_ff_~a_~a (void *);\n"
+                  struct-name field-name)
+          (format #t "\n")       
+          )
+         (('field (? symbol? field-type) (? string? struct-name)
+                  (? string? field-name) (? integer? offset) (? integer? size))
+          (format #t "VISIBLE ~a get_ff_~a_~a (void *);\n"
+                  (value-c-type field-type size) struct-name field-name)
+          (format #t "VISIBLE void set_ff_~a_~a (void *, ~a);\n"
+                  struct-name field-name (value-c-type field-type size))
+          (format #t "VISIBLE void *ptr_ff_~a_~a (void *);\n"
+                  struct-name field-name)
+          (format #t "\n")
+          )
+         (('field ((and (or '* 'struct 'array ) field-type) (? symbol? pointer-type))
+                  (? string? struct-name) (? string? field-name) (? integer? offset)
+                  (? integer? size))
+          (write-instruction (list 'field field-type struct-name field-name offset size))
+          ;;
+          ;; FIXME: Dereferencing and array procedures go here.
+          ;;
+          )
+         
+         ((instruction-symbol . _)
+          (format (current-error-port) "Ignoring '~a\n" instruction-symbol))
+         ))
 
 (define (value-c-type field-type size)
-   (match (cons field-type size)
-      (('int . 1) "int")
-      (('int . 2) "int")
-      (('int . 4) "int")
-      (('int . 8) "int64_t")
-      (('uint . 1) "unsigned int")
-      (('uint . 2) "unsigned int")
-      (('uint . 4) "unsigned int")
-      (('uint . 8) "uint64_t")
-      (('bool . _) "bool")
-      (('float . _) "double")
-      (('* . _) "void *")
-      (('struct . _) (error "NOT YET IMPLEMENTED"))
-      (('array . _) (error "NOT YET IMPLEMENTED"))
-      ))
+  (match (cons field-type size)
+         (('int . 1) "int")
+         (('int . 2) "int")
+         (('int . 4) "int")
+         (('int . 8) "int64_t")
+         (('uint . 1) "unsigned int")
+         (('uint . 2) "unsigned int")
+         (('uint . 4) "unsigned int")
+         (('uint . 8) "uint64_t")
+         (('bool . _) "bool")
+         (('float . _) "double")
+         (('* . _) "void *")
+         (('struct . _) (error "NOT YET IMPLEMENTED"))
+         (('array . _) (error "NOT YET IMPLEMENTED"))
+         ))
 
 (let ((instructions (read-instructions-from-program-input)))
-   (format #t "/* Generated by ~s */\n" (car (command-line)))
-   (format #t "\n")
-   (format #t "#include <config.h>\n")
-   (format #t "#include <sortsmillff/fontforge_api.h>\n")
-   (format #t "\n")
-   (format #t "/* Extern-addressable API functions. */\n")
-   (format #t "\n")
-   (for-each write-instruction instructions)
-   )
+  (format #t "/* Generated by ~s */\n" (car (command-line)))
+  (format #t "\n")
+  (format #t "#include <config.h>\n")
+  (format #t "#include <sortsmillff/fontforge_api.h>\n")
+  (format #t "\n")
+  (format #t "/* Extern-addressable API functions. */\n")
+  (format #t "\n")
+  (for-each write-instruction instructions)
+  )
--- fontforge/generate-c-api.h.scm
+++ fontforge/generate-c-api.h.scm
@@ -1,4 +1,4 @@
-#! @GUILE@ \           -*- mode: bee; coding: utf-8 -*-
+#! @GUILE@ \           -*- mode: scheme; coding: utf-8 -*-
 --no-auto-compile -s
 !#
 
@@ -17,145 +17,144 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-(use-modules
-   (ff-internal generate-types)
-   (sortsmillff machine)
-   (ice-9 match)
-   (ice-9 format)
-   )
+(import (ff-internal generate-types)
+        (sortsmillff machine)
+        (ice-9 match)
+        (ice-9 format)
+        )
 
 (define (write-instruction instruction)
-   (match instruction
-      (('struct (? string? struct-name) (? integer? size))
-       (format #t "inline void *malloc_ff_~a (void);\n" struct-name)
-       (format #t "inline void *malloc_ff_~a (void) { return xdie_on_null (calloc (1, ~d)); }\n"
-          struct-name size)
-       (format #t "\n")
-       (format #t "inline void free_ff_~a (void *);\n" struct-name)
-       (format #t "inline void free_ff_~a (void *p) { free (p); }\n" struct-name)
-       (format #t "\n")
-       (format #t "inline void *gc_malloc_ff_~a (void);\n" struct-name)
-       (format #t "inline void *gc_malloc_ff_~a (void) { return x_gc_malloc (~d); }\n"
-          struct-name size)
-       (format #t "\n")
-       (format #t "inline void gc_free_ff_~a (void *);\n" struct-name)
-       (format #t "inline void gc_free_ff_~a (void *p) { GC_FREE (p); }\n" struct-name)
-       (format #t "\n")
-       )
-      (('sizeof (? string? struct-name) (? integer? size))
-       (format #t "inline size_t sizeof_ff_~a (void);\n" struct-name)
-       (format #t "inline size_t sizeof_ff_~a (void) { return ~d; };\n" struct-name size)
-       (format #t "\n")
-       )
-      (('field (and (or 'struct 'array) field-type) (? string? struct-name)
-          (? string? field-name) (? integer? offset) (? integer? size))
-       (format #t "inline void *ptr_ff_~a_~a (void *);\n"
-          struct-name field-name)
-       (format #t "inline void *ptr_ff_~a_~a (void *p) { return (void *) &((char *) p)[~d]; }\n"
-          struct-name field-name offset)
-       (format #t "\n")
-       )
-      (('field (? symbol? field-type) (? string? struct-name)
-          (? string? field-name) (? integer? offset) (? integer? size))
-       (format #t "inline ~a get_ff_~a_~a (void *);\n"
-          (value-c-type field-type size) struct-name field-name)
-       (format #t "inline ~a get_ff_~a_~a (void *p) { return ~a; }\n"
-          (value-c-type field-type size) struct-name field-name
-          (get-value-expression field-type offset size "p"))
-       (format #t "\n")
-       (format #t "inline void set_ff_~a_~a (void *, ~a);\n"
-          struct-name field-name (value-c-type field-type size))
-       (format #t "inline void set_ff_~a_~a (void *p, ~a v) { ~a; }\n"
-          struct-name field-name (value-c-type field-type size)
-          (set-value-expression field-type offset size "p" "v"))
-       (format #t "\n")
-       (format #t "inline void *ptr_ff_~a_~a (void *);\n"
-          struct-name field-name)
-       (format #t "inline void *ptr_ff_~a_~a (void *p) { return (void *) &((char *) p)[~d]; }\n"
-          struct-name field-name offset)
-       (format #t "\n")
-       )
-      (('field ((and (or '* 'struct 'array) field-type) (? symbol? field-subtype))
-          (? string? struct-name) (? string? field-name) (? integer? offset) (? integer? size))
-       (write-instruction (list 'field field-type struct-name field-name offset size))
-       ;;
-       ;; FIXME: Dereferencing and array procedures go here.
-       ;;
-       )
+  (match instruction
+         (('struct (? string? struct-name) (? integer? size))
+          (format #t "inline void *malloc_ff_~a (void);\n" struct-name)
+          (format #t "inline void *malloc_ff_~a (void) { return xdie_on_null (calloc (1, ~d)); }\n"
+                  struct-name size)
+          (format #t "\n")
+          (format #t "inline void free_ff_~a (void *);\n" struct-name)
+          (format #t "inline void free_ff_~a (void *p) { free (p); }\n" struct-name)
+          (format #t "\n")
+          (format #t "inline void *gc_malloc_ff_~a (void);\n" struct-name)
+          (format #t "inline void *gc_malloc_ff_~a (void) { return x_gc_malloc (~d); }\n"
+                  struct-name size)
+          (format #t "\n")
+          (format #t "inline void gc_free_ff_~a (void *);\n" struct-name)
+          (format #t "inline void gc_free_ff_~a (void *p) { GC_FREE (p); }\n" struct-name)
+          (format #t "\n")
+          )
+         (('sizeof (? string? struct-name) (? integer? size))
+          (format #t "inline size_t sizeof_ff_~a (void);\n" struct-name)
+          (format #t "inline size_t sizeof_ff_~a (void) { return ~d; };\n" struct-name size)
+          (format #t "\n")
+          )
+         (('field (and (or 'struct 'array) field-type) (? string? struct-name)
+                  (? string? field-name) (? integer? offset) (? integer? size))
+          (format #t "inline void *ptr_ff_~a_~a (void *);\n"
+                  struct-name field-name)
+          (format #t "inline void *ptr_ff_~a_~a (void *p) { return (void *) &((char *) p)[~d]; }\n"
+                  struct-name field-name offset)
+          (format #t "\n")
+          )
+         (('field (? symbol? field-type) (? string? struct-name)
+                  (? string? field-name) (? integer? offset) (? integer? size))
+          (format #t "inline ~a get_ff_~a_~a (void *);\n"
+                  (value-c-type field-type size) struct-name field-name)
+          (format #t "inline ~a get_ff_~a_~a (void *p) { return ~a; }\n"
+                  (value-c-type field-type size) struct-name field-name
+                  (get-value-expression field-type offset size "p"))
+          (format #t "\n")
+          (format #t "inline void set_ff_~a_~a (void *, ~a);\n"
+                  struct-name field-name (value-c-type field-type size))
+          (format #t "inline void set_ff_~a_~a (void *p, ~a v) { ~a; }\n"
+                  struct-name field-name (value-c-type field-type size)
+                  (set-value-expression field-type offset size "p" "v"))
+          (format #t "\n")
+          (format #t "inline void *ptr_ff_~a_~a (void *);\n"
+                  struct-name field-name)
+          (format #t "inline void *ptr_ff_~a_~a (void *p) { return (void *) &((char *) p)[~d]; }\n"
+                  struct-name field-name offset)
+          (format #t "\n")
+          )
+         (('field ((and (or '* 'struct 'array) field-type) (? symbol? field-subtype))
+                  (? string? struct-name) (? string? field-name) (? integer? offset) (? integer? size))
+          (write-instruction (list 'field field-type struct-name field-name offset size))
+          ;;
+          ;; FIXME: Dereferencing and array procedures go here.
+          ;;
+          )
 
-      ((instruction-symbol . _)
-       (format (current-error-port) "Ignoring '~a\n" instruction-symbol))
-      ))
+         ((instruction-symbol . _)
+          (format (current-error-port) "Ignoring '~a\n" instruction-symbol))
+         ))
 
 (define (value-c-type field-type size)
-   (match (cons field-type size)
-      (('int . 1) "int")
-      (('int . 2) "int")
-      (('int . 4) "int")
-      (('int . 8) "int64_t")
-      (('uint . 1) "unsigned int")
-      (('uint . 2) "unsigned int")
-      (('uint . 4) "unsigned int")
-      (('uint . 8) "uint64_t")
-      (('bool . _) "bool")
-      (('float . _) "double")
-      (('* . _) "void *")
-      (('struct . _) (error "NOT YET IMPLEMENTED"))
-      (('array . _) (error "NOT YET IMPLEMENTED"))
-      ))
+  (match (cons field-type size)
+         (('int . 1) "int")
+         (('int . 2) "int")
+         (('int . 4) "int")
+         (('int . 8) "int64_t")
+         (('uint . 1) "unsigned int")
+         (('uint . 2) "unsigned int")
+         (('uint . 4) "unsigned int")
+         (('uint . 8) "uint64_t")
+         (('bool . _) "bool")
+         (('float . _) "double")
+         (('* . _) "void *")
+         (('struct . _) (error "NOT YET IMPLEMENTED"))
+         (('array . _) (error "NOT YET IMPLEMENTED"))
+         ))
 
 (define (get-value-expression field-type offset size pointer-expression)
-   (let ((address (format #f "((void *) &((char *) (~a))[~d])"
-                     pointer-expression offset)))
-      (match (cons field-type size)
-         (('int . n) (format #f "(*(~a *) ~a)" (c-int-type n) address))
-         (('uint . n) (format #f "(*(~a *) ~a)" (c-uint-type n) address))
-         (('bool . n) (format #f "((bool) (*(~a *) ~a != 0))" (c-uint-type n) address))
-         (('float . n) (format #f "(*(~a *) ~a)" (c-float-type n) address))
-         (('* . n) (format #f "((void *) *(~a *) ~a)" (c-uint-type n) address))
-         (('struct . _) (error "NOT YET IMPLEMENTED"))
-         (('array . _) (error "NOT YET IMPLEMENTED"))
-         )))
+  (let ((address (format #f "((void *) &((char *) (~a))[~d])"
+                         pointer-expression offset)))
+    (match (cons field-type size)
+           (('int . n) (format #f "(*(~a *) ~a)" (c-int-type n) address))
+           (('uint . n) (format #f "(*(~a *) ~a)" (c-uint-type n) address))
+           (('bool . n) (format #f "((bool) (*(~a *) ~a != 0))" (c-uint-type n) address))
+           (('float . n) (format #f "(*(~a *) ~a)" (c-float-type n) address))
+           (('* . n) (format #f "((void *) *(~a *) ~a)" (c-uint-type n) address))
+           (('struct . _) (error "NOT YET IMPLEMENTED"))
+           (('array . _) (error "NOT YET IMPLEMENTED"))
+           )))
 
 (define (set-value-expression field-type offset size pointer-expression
-           value-expression)
-   (let ((address (format #f "((void *) &((char *) (~a))[~d])"
-                     pointer-expression offset)))
-      (match (cons field-type size)
-         (('int . n) (format #f "(*(~a *) ~a = (~a))"
-                        (c-int-type n) address value-expression))
-         (('uint . n) (format #f "(*(~a *) ~a = (~a))"
-                         (c-uint-type n) address value-expression))
-         (('bool . n) (format #f "(*(~a *) ~a = ((~a) != 0))"
-                         (c-uint-type n) address value-expression))
-         (('float . n) (format #f "(*(~a *) ~a = (~a))"
-                          (c-float-type n)  address value-expression))
-         (('* . n) (format #f "(*(~a *) ~a = (uint8_t) (uintptr_t) (~a))"
-                      (c-uint-type n) address value-expression))
-         (('struct . _) (error "NOT YET IMPLEMENTED"))
-         (('array . _) (error "NOT YET IMPLEMENTED"))
-         )))
+                              value-expression)
+  (let ((address (format #f "((void *) &((char *) (~a))[~d])"
+                         pointer-expression offset)))
+    (match (cons field-type size)
+           (('int . n) (format #f "(*(~a *) ~a = (~a))"
+                               (c-int-type n) address value-expression))
+           (('uint . n) (format #f "(*(~a *) ~a = (~a))"
+                                (c-uint-type n) address value-expression))
+           (('bool . n) (format #f "(*(~a *) ~a = ((~a) != 0))"
+                                (c-uint-type n) address value-expression))
+           (('float . n) (format #f "(*(~a *) ~a = (~a))"
+                                 (c-float-type n)  address value-expression))
+           (('* . n) (format #f "(*(~a *) ~a = (uint8_t) (uintptr_t) (~a))"
+                             (c-uint-type n) address value-expression))
+           (('struct . _) (error "NOT YET IMPLEMENTED"))
+           (('array . _) (error "NOT YET IMPLEMENTED"))
+           )))
 
 (let ((instructions (read-instructions-from-program-input)))
-   (format #t "/* Generated by ~s */\n" (car (command-line)))
-   (format #t "\n")
-   (format #t "#ifndef SORTSMILLFF_INTERNAL_TYPES_H\n")
-   (format #t "#define SORTSMILLFF_INTERNAL_TYPES_H\n")
-   (format #t "\n")
-   (format #t "#include <stdlib.h>\n")
-   (format #t "#include <stdint.h>\n")
-   (format #t "#include <stdbool.h>\n")
-   (format #t "#include <sortsmillff/xgc.h>\n")
-   (format #t "#include <sortsmillff/xdie_on_null.h>\n")
-   (format #t "\n")
-   (format #t "#ifdef __cplusplus\n")
-   (format #t "extern \"C\" {\n")
-   (format #t "#endif\n")
-   (format #t "\n")
-   (for-each write-instruction instructions)
-   (format #t "#ifdef __cplusplus\n")
-   (format #t "}\n")
-   (format #t "#endif\n")
-   (format #t "\n")
-   (format #t "#endif /* SORTSMILLFF_INTERNAL_TYPES_H */\n")
-   )
+  (format #t "/* Generated by ~s */\n" (car (command-line)))
+  (format #t "\n")
+  (format #t "#ifndef SORTSMILLFF_INTERNAL_TYPES_H\n")
+  (format #t "#define SORTSMILLFF_INTERNAL_TYPES_H\n")
+  (format #t "\n")
+  (format #t "#include <stdlib.h>\n")
+  (format #t "#include <stdint.h>\n")
+  (format #t "#include <stdbool.h>\n")
+  (format #t "#include <sortsmillff/xgc.h>\n")
+  (format #t "#include <sortsmillff/xdie_on_null.h>\n")
+  (format #t "\n")
+  (format #t "#ifdef __cplusplus\n")
+  (format #t "extern \"C\" {\n")
+  (format #t "#endif\n")
+  (format #t "\n")
+  (for-each write-instruction instructions)
+  (format #t "#ifdef __cplusplus\n")
+  (format #t "}\n")
+  (format #t "#endif\n")
+  (format #t "\n")
+  (format #t "#endif /* SORTSMILLFF_INTERNAL_TYPES_H */\n")
+  )
