cab3ce1692693adfeb043ef108f096827c0dc73f
Ben Martin <monkeyiq@users.sourceforge.net>
2013-01-18 14:26:19 +0200

    Fix for crash when connecting two spiro points
    The CVMergeSPLS() function is only called in this particular case and
    was assuming memory ownership in cv->p.spl which might not have been
    true.

--- fontforge/cvaddpoints.c
+++ fontforge/cvaddpoints.c
@@ -306,6 +306,7 @@ static void CVMergeSPLS(CharView *cv,SplineSet *ss, SplinePoint *base,SplinePoin
     } else
 	SplineSetSpirosClear(ss);
     cv->p.spl->last = cv->p.spl->first = NULL;
+    cv->p.spl->spiros = 0;
     SplinePointListFree(cv->p.spl);
     cv->p.spl = NULL;
 }
