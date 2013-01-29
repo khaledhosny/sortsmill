1107faf23c999ea22a054883bb26990dc48e8815
Barry Schwartz <github@crudfactory.com>
2013-01-29 12:21:33 -0600

    Have configure check for scm_init_guile() support, and require it if Python is desired.

--- python/sortsmill/guile.pyx
+++ python/sortsmill/guile.pyx
@@ -15,12 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-from sortsmill.cython.guile cimport scm_init_guile
+from sortsmill.cython.guile cimport SCM
+cimport sortsmill.cython.guile as scm
 
 #--------------------------------------------------------------------------
 
-# FIXME: Test whether scm_init_guile() is available on the system.
 def init_guile ():
-  scm_init_guile ()
+  scm.scm_init_guile ()
 
 #--------------------------------------------------------------------------
