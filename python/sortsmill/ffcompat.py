dd71f2f8de96f4348338d7980953961f0325a31f
Barry Schwartz <github@crudfactory.com>
2013-02-01 22:32:30 -0600

    Put GPL-3+ license notice in psMat and ffcompat sources.

--- python/sortsmill/ffcompat.py
+++ python/sortsmill/ffcompat.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8; python-indent: 2; -*-
 
 # Copyright (C) 2012, 2013 by Barry Schwartz
-# Based in part on python.c by George Williams, which is
-#   Copyright (C) 2007-2012 by George Williams
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# Based in part on python.c by George Williams, which is copyright and
+# licensed as follows:
+#
+# Copyright (C) 2007-2012 by George Williams
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
