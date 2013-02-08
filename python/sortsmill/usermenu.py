f31e2171f6f97e2118f4edf46f7b643d32b5bae3
Barry Schwartz <github@crudfactory.com>
2013-02-07 21:13:16 -0600

    Preconditions in usermenu.py.

--- python/sortsmill/usermenu.py
+++ python/sortsmill/usermenu.py
@@ -15,16 +15,52 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-from . import guile
+from . import (guile, conditions)
 
-# FIXME: Get rid of this or revise it for the menu design we end up
-# with.
+
+def __force_unicode (s):
+  if isinstance (s, bytes):
+    s = s.decode ('UTF-8')
+  return s
+
+__window_cases = ('font', 'glyph', 'char')
+
+def __acceptable_as_window (w):
+  global __window_cases
+  w = __force_unicode (w)
+  return isinstance (w, unicode) and w.lower () in __window_cases
+
+def __acceptable_as_menu_path (mp):
+  try:
+    acceptable = all ([isinstance (__force_unicode (s), unicode) for s in mp])
+  except:
+    acceptable = False
+  return acceptable
+
+def __register_fontforge_menu_entry_preconditions (window, menu_path, action,
+                                                   enabled = None,
+                                                   shortcut = None):
+  result = True
+  if not __acceptable_as_window (window):
+    result = 'expected one of {} for ‘window’, but got: {}'.format (__window_cases, window)
+  elif not __acceptable_as_menu_path (menu_path):
+    result = 'expected a string sequence for ‘menu_path’, but got: {}'.format (menu_path)
+  elif not callable (action):
+    result = '‘action’ is not callable: {}'.format (action)
+  elif enabled is not None and not callable (enabled):
+    result = '‘enabled’ is not callable: {}'.format (enabled)
+  elif shortcut is not None and not isinstance (__force_unicode (shortcut), unicode):
+    result = '‘shortcut’ is not a string: {}'.format (shortcut)
+  return result
+
+@conditions.pre (__register_fontforge_menu_entry_preconditions)
 def register_fontforge_menu_entry (window, menu_path, action,
                                    enabled = None,
                                    shortcut = None):
-  assert window is not None
-  assert menu_path is not None
-  assert action is not None
+
+  # FIXME: Get rid of this or revise it for the menu design we end up
+  # with.
+
   guile.call (guile.public_ref ('sortsmill usermenu python',
                                 'register-python-menu-entry'),
               guile.pyobject (window),
