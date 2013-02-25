5b748a175093fbe0da29430d6a4145d3b557d5a8
Barry Schwartz <github@crudfactory.com>
2013-02-24 19:28:14 -0600

    More tests for scm_matrix.[ch].
    --HG--
    rename : tests/test_scm_matrix_swap.c => tests/test_scm_matrix_add.c
    rename : tests/test_scm_matrix_scale.c => tests/test_scm_matrix_add_constant.c
    rename : tests/test_scm_matrix_swap.c => tests/test_scm_matrix_div_elements.c
    rename : tests/test_scm_matrix_swap.c => tests/test_scm_matrix_mul_elements.c
    rename : tests/test_scm_matrix_swap.c => tests/test_scm_matrix_sub.c

---
+++ tests/test_scm_matrix_add_constant.c
@@ -0,0 +1,47 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sortsmill/xgc.h>
+#include <locale.h>
+#include <float.h>
+#include <math.h>
+#include <libguile.h>
+#include <sortsmill/scm_matrix.h>
+
+#include <main_with_guile.x>
+
+static int
+my_main (int argc, char **argv)
+{
+  setlocale (LC_ALL, "");
+
+  int rows = atoi (argv[1]);
+  int cols = atoi (argv[2]);
+
+  SCM x = scm_c_locale_stringn_to_number (argv[3], strlen (argv[3]), 10);
+
+  SCM A[rows][cols];
+
+  unsigned int i_argv = 4;
+
+  for (unsigned int i = 0; i < rows; i++)
+    for (unsigned int j = 0; j < cols; j++)
+      {
+        A[i][j] = scm_c_locale_stringn_to_number (argv[i_argv],
+                                                  strlen (argv[i_argv]), 10);
+        i_argv++;
+      }
+
+  scm_matrix_add_constant (rows, cols, A, x);
+
+  for (unsigned int i = 0; i < rows; i++)
+    {
+      for (unsigned int j = 0; j < cols; j++)
+        scm_simple_format (SCM_BOOL_T, scm_from_latin1_string (" ~a"),
+                           scm_list_1 (A[i][j]));
+      scm_simple_format (SCM_BOOL_T, scm_from_latin1_string (" |"), SCM_EOL);
+    }
+
+  return 0;
+}
---
+++ tests/test_scm_matrix_sub.c
@@ -0,0 +1,64 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sortsmill/xgc.h>
+#include <locale.h>
+#include <float.h>
+#include <math.h>
+#include <libguile.h>
+#include <sortsmill/scm_matrix.h>
+
+#include <main_with_guile.x>
+
+static int
+my_main (int argc, char **argv)
+{
+  setlocale (LC_ALL, "");
+
+  int rows = atoi (argv[1]);
+  int cols = atoi (argv[2]);
+
+  SCM A[rows][cols];
+  SCM B[rows][cols];
+
+  unsigned int i_argv = 3;
+
+  for (unsigned int i = 0; i < rows; i++)
+    for (unsigned int j = 0; j < cols; j++)
+      {
+        A[i][j] = scm_c_locale_stringn_to_number (argv[i_argv],
+                                                  strlen (argv[i_argv]), 10);
+        i_argv++;
+      }
+
+  for (unsigned int i = 0; i < rows; i++)
+    for (unsigned int j = 0; j < cols; j++)
+      {
+        B[i][j] = scm_c_locale_stringn_to_number (argv[i_argv],
+                                                  strlen (argv[i_argv]), 10);
+        i_argv++;
+      }
+
+  scm_matrix_sub (rows, cols, A, B);
+
+  for (unsigned int i = 0; i < rows; i++)
+    {
+      for (unsigned int j = 0; j < cols; j++)
+        scm_simple_format (SCM_BOOL_T, scm_from_latin1_string (" ~a"),
+                           scm_list_1 (A[i][j]));
+      scm_simple_format (SCM_BOOL_T, scm_from_latin1_string (" |"), SCM_EOL);
+    }
+
+  scm_simple_format (SCM_BOOL_T, scm_from_latin1_string (" *** |"), SCM_EOL);
+
+  for (unsigned int i = 0; i < rows; i++)
+    {
+      for (unsigned int j = 0; j < cols; j++)
+        scm_simple_format (SCM_BOOL_T, scm_from_latin1_string (" ~a"),
+                           scm_list_1 (B[i][j]));
+      scm_simple_format (SCM_BOOL_T, scm_from_latin1_string (" |"), SCM_EOL);
+    }
+
+  return 0;
+}
