c0a8c1924066d66204e0df0d1ef66b946fd2e850
Barry Schwartz <github@crudfactory.com>
2013-03-04 08:40:56 -0600

    As long as we have the data already printed out, let’s add some precomputed values to bincoef.c.

--- auxiliary/bincoef.c
+++ auxiliary/bincoef.c
@@ -21,6 +21,20 @@
 //
 // See http://en.wikipedia.org/wiki/Binomial_coefficient
 
+static uint8_t precomputed_bincoef[11][11] = {
+  { 1 },
+  { 1, 1 },
+  { 1, 2, 1 },
+  { 1, 3, 3, 1 },
+  { 1, 4, 6, 4, 1 },
+  { 1, 5, 10, 10, 5, 1 },
+  { 1, 6, 15, 20, 15, 6, 1 },
+  { 1, 7, 21, 35, 35, 21, 7, 1 },
+  { 1, 8, 28, 56, 70, 56, 28, 8, 1 },
+  { 1, 9, 36, 84, 126, 126, 84, 36, 9, 1 },
+  { 1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1 }
+};
+
 VISIBLE uintmax_t
 bincoef (uintmax_t n, uintmax_t k)
 {
@@ -28,6 +42,8 @@ bincoef (uintmax_t n, uintmax_t k)
 
   if (n < k)
     C = 0;
+  else if (n <= 10)
+    C = precomputed_bincoef[n][k];
   else
     {
       // Use C(n,k) ≡ C(n, n − k) ≡ C(n, min(k, n − k)) to improve
