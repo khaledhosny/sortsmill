433a9d63022139e1057f5e0217044eb4b251abea
Barry Schwartz <github@crudfactory.com>
2013-03-08 09:31:03 -0600

    Get rid of mpqmat stuff. I give up.

--- inc/sortsmill/guile/math.h
+++ inc/sortsmill/guile/math.h
@@ -22,7 +22,6 @@
 #include <sortsmill/guile/math/functions.h>
 #include <sortsmill/guile/math/gmp.h>
 #include <sortsmill/guile/math/gsl.h>
-#include <sortsmill/guile/math/matrices.h>
 #include <sortsmill/guile/math/polyspline.h>
 
 #endif /* _SORTSMILL_GUILE_MATH_H */
