ea6266f8768396e78f61d3c47d1b96d814897e74
Barry Schwartz <github@crudfactory.com>
2013-03-15 12:12:43 -0500

    Floating point scaled Bernstein polynomial composition.

--- auxiliary/math_polyspline_compose.c
+++ auxiliary/math_polyspline_compose.c
@@ -61,6 +61,50 @@ compose_scm_mono (size_t degree_a, ssize_t stride_a, const SCM *a,
   copy_scm_with_strides (stride_c, c, 1, _c, degree_c + 1);
 }
 
+VISIBLE void
+compose_f64_sbern (size_t degree_a, ssize_t stride_a, const double *a,
+                   size_t degree_b, ssize_t stride_b, const double *b,
+                   ssize_t stride_c, double *c)
+{
+  // Use a simple modified Horner scheme, with polynomial values.
+  //
+  // (De Casteljau’s algorithm also would work but is expensive.  The
+  // Schumaker-Volk algorithm would require rational functions.)
+
+  double one_minus_a[degree_a + 1];
+  one_minus_f64_sbern (degree_a, stride_a, a, 1, one_minus_a);
+
+  // degree_c = the degree of the result.
+  const size_t degree_c = degree_a * degree_b;
+
+  double power_of_one_minus_a[degree_c + 1];
+  memcpy (power_of_one_minus_a, one_minus_a, (degree_a + 1) * sizeof (double));
+
+  double accumulator[degree_c + 1];
+  accumulator[0] = b[stride_b * (ssize_t) degree_b];
+
+  double new_term[degree_c + 1];
+  for (size_t i = 1; i <= degree_b; i++)
+    {
+      mul_f64_sbern ((i - 1) * degree_a, 1, accumulator,
+                     degree_a, stride_a, a, 1, accumulator);
+
+      memcpy (new_term, power_of_one_minus_a,
+              (i * degree_a + 1) * sizeof (double));
+      for (size_t j = 0; j <= i * degree_a; j++)
+        new_term[j] *= b[stride_b * (ssize_t) (degree_b - i)];
+
+      add_f64_splines (i * degree_a, 1, accumulator, 1, new_term, 1,
+                       accumulator);
+
+      if (i < degree_b)
+        mul_f64_sbern (i * degree_a, 1, power_of_one_minus_a,
+                       degree_a, 1, one_minus_a, 1, power_of_one_minus_a);
+    }
+
+  copy_f64_with_strides (stride_c, c, 1, accumulator, degree_c + 1);
+}
+
 static void
 compose_f64_spower_odd_degree (size_t degree_a,
                                ssize_t stride_a, const double *a,
@@ -238,6 +282,12 @@ scm_compose_f64_mono (SCM a, SCM b)
                                  compose_f64_mono, a, b);
 }
 
+VISIBLE SCM
+scm_compose_f64_sbern (SCM a, SCM b)
+{
+  return scm_compose_f64_spline ("scm_compose_f64_sbern",
+                                 compose_f64_sbern, a, b);
+}
 
 VISIBLE SCM
 scm_compose_f64_spower (SCM a, SCM b)
@@ -321,7 +371,7 @@ init_math_polyspline_compose (void)
   //scm_c_define_gsubr ("poly:compose-f64-bern", 2, 0, 0, scm_compose_f64_bern);
   //scm_c_define_gsubr ("poly:compose-scm-bern", 2, 0, 0, scm_compose_scm_bern);
 
-  //scm_c_define_gsubr ("poly:compose-f64-sbern", 2, 0, 0, scm_compose_f64_sbern);
+  scm_c_define_gsubr ("poly:compose-f64-sbern", 2, 0, 0, scm_compose_f64_sbern);
   //scm_c_define_gsubr ("poly:compose-scm-sbern", 2, 0, 0, scm_compose_scm_sbern);
 
   scm_c_define_gsubr ("poly:compose-f64-spower", 2, 0, 0,
