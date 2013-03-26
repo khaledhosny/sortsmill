86c4aa62c387a659f1ba11aa00bb7322f1fbb5ac
Barry Schwartz <github@crudfactory.com>
2013-03-26 00:59:40 -0500

    Spline inversion is implemented up to degree 3, which is as high as we are going to go.

--- guile/sortsmill/math/polyspline/inversion.scm
+++ guile/sortsmill/math/polyspline/inversion.scm
@@ -34,9 +34,11 @@
   (define/kwargs (poly:invert-spline-mono xspline yspline t1 t2 x y)
     "Given a spline in monomial basis, and a point (x,y), try to find
 the values of the parameter t that correspond to (x,y) and lie in the
-interval [t1,t2]. The results, which are returned as a list, are
-approximate in general. If the spline effectively is a point, then the
-symbol @code{point} is returned instead of a list of t values."
+interval [t1,t2]. The results, which are returned as a list, in
+general are approximate and not confined within brackets, so you may
+want to set [t1,t2] a little wide. If the spline effectively is a
+point, then the symbol @code{point} is returned instead of a list of t
+values."
     (assert (<= t1 t2))
     (let* ([min-degree (max (poly:min-degree-scm-mono xspline)
                             (poly:min-degree-scm-mono yspline))]
@@ -84,31 +86,56 @@ situation."
       (let ([B (bezout-matrix x-xspline y-yspline)])
         (let-values ([(U S V) (f64matrix-svd (matrix->f64matrix B))])
           (case (matrix-svd-effective-rank S)
-            [(0) (invert-quadratic-rank0 xspline yspline t1 t2 x y)]
-            [(1) (invert-quadratic-rank1 V t1 t2)]
-            [(2)
-             ;; Assume we have a lot of round-off and the rank really
-             ;; is one.
-             (invert-quadratic-rank1 V t1 t2)] )))))
+            [(0) (invert-rank0 xspline t1 t2 x)]
+            [(1 2)
+             ;; If the effective rank is 2, assume we have a lot of
+             ;; round-off and the rank really is 1.
+             (invert-quadratic-rank1 V t1 t2)])))))
 
-  (define (invert-quadratic-rank0 xspline yspline t1 t2 x y)
+  (define (invert-rank0 xspline t1 t2 x)
     (let ([x-xspline (zero-based
                       (matrix-inexact->exact
                        (poly:sub-scm-mono (make-vector 1 x) xspline)))])
-      (match x-xspline
-        [#(x0 x1 x2) (poly:find-roots-scm-mono x-xspline t1 t2)])))
+      (poly:find-roots-scm-mono x-xspline t1 t2)))
 
   (define (invert-quadratic-rank1 V t1 t2)
     "The right column of V is a basis of the null space of the Bézout
 matrix, and thus (for our definition of that matrix) is proportional
 to the transpose of @code{#(1 t)}. (Other definitions of the Bézout
-matrix may vary by a factor or in the order of entries.)"
+matrix may vary by a scalar factor or in the order of entries.)"
     (let ([root (/ (array-ref V 2 2) (array-ref V 1 2))])
       (if (<= t1 root t2)
           (list root)
           '())))
 
   (define (invert-cubic xspline yspline t1 t2 x y)
-    (assertion-violation 'invert-cubic "not yet implemented"))
+    ;; Construct a Bézout matrix and compute its singular value
+    ;; decomposition.
+    (let ([x-xspline (poly:sub-scm-mono (make-vector 1 x) xspline)]
+          [y-yspline (poly:sub-scm-mono (make-vector 1 y) yspline)])
+      (let ([B (bezout-matrix x-xspline y-yspline)])
+        (let-values ([(U S V) (f64matrix-svd (matrix->f64matrix B))])
+          (case (matrix-svd-effective-rank S)
+            [(0) (invert-rank0 xspline t1 t2 x)]
+            [(1) (invert-cubic-rank1 V t1 t2)]
+            [(2 3)
+             ;; If the effective rank is 3, assume we have a lot of
+             ;; round-off and the rank really is 2.
+             (invert-cubic-rank2 V t1 t2)] )))))
+
+  (define (invert-cubic-rank1 V t1 t2)
+    "Solve (v₁₁ v₁₂ v₁₃)⋅(1 t t²)ᵀ = 0."
+    (let ([poly (f64matrix->matrix (matrix-row V 1))])
+      (poly:find-roots-scm-mono poly t1 t2)))
+
+  (define (invert-cubic-rank2 V t1 t2)
+    "The right column of V is a basis of the null space of the Bézout
+matrix, and thus (for our definition of that matrix) is proportional
+to the transpose of @code{#(1 t t²)}. (Other definitions of the Bézout
+matrix may vary by a scalar factor or in the order of entries.)"
+    (let ([root (/ (array-ref V 2 3) (array-ref V 1 3))])
+      (if (<= t1 root t2)
+          (list root)
+          '())))
 
   ) ;; end of library.
