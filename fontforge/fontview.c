10a2da64b79b743add139956db1add5ddd6b6d0d
Khaled Hosny <khaledhosny@eglug.org>
2013-04-04 16:55:50 +0200

    Pass true to SFBaselines() for vertical base line
    Else the title is wrong.

--- fontforge/fontview.c
+++ fontforge/fontview.c
@@ -3195,7 +3195,7 @@ FVMenuBaseVert (GWindow gw, struct gmenuitem *UNUSED (mi), GEvent *UNUSED (e))
 {
   FontView *fv = (FontView *) GDrawGetUserData (gw);
   SplineFont *sf = fv->b.cidmaster == NULL ? fv->b.sf : fv->b.cidmaster;
-  sf->vert_base = SFBaselines (sf, sf->vert_base, false);
+  sf->vert_base = SFBaselines (sf, sf->vert_base, true);
   SFBaseSort (sf);
 }
 
