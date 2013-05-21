6869b3d4351ed225a38f5d3a16d2f74bc4956aff
Barry Schwartz <github@crudfactory.com>
2013-05-20 23:15:52 -0500

    Put GPL3+ notices on fontforge/b* files

--- fontforge/bezctx.h
+++ fontforge/bezctx.h
@@ -1,19 +1,17 @@
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 3
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-02110-1301, USA.
-*/
+// This file is part of the Sorts Mill Tools.
+// 
+// Sorts Mill Tools is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Sorts Mill Tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 #ifndef _FF_INTERNAL_BEZCTX_H
 #define _FF_INTERNAL_BEZCTX_H
