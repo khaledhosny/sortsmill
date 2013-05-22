7b3b5c628eeebec686e2e18876c24dea28d5de37
Barry Schwartz <github@crudfactory.com>
2013-05-22 00:04:09 -0500

    gpl notices on files in the python directory.

--- python/sortsmill/psMat.py
+++ python/sortsmill/psMat.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8; python-indent: 2; -*-
 
-# Copyright (C) 2012, 2013 by Barry Schwartz
+# Copyright (C) 2012, 2013 Khaled Hosny and Barry Schwartz
+# This file is part of the Sorts Mill Tools.
 # 
-# This program is free software; you can redistribute it and/or modify
+# Sorts Mill Tools is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 # 
-# This program is distributed in the hope that it will be useful,
+# Sorts Mill Tools is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -15,9 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-# Based in part on python.c by George Williams, for which copyright
-# and license are as follows:
-#
 # Copyright (C) 2007-2012 by George Williams
 #
 # Redistribution and use in source and binary forms, with or without
