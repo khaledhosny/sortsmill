d59cb1fa10ed903f7dc7919592b60bbffe4e89ba
Barry Schwartz <github@crudfactory.com>
2013-05-22 00:57:42 -0500

    GPL notices in the guile directory.

--- guile/ff-internal/generate-types.scm
+++ guile/ff-internal/generate-types.scm
@@ -1,13 +1,14 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 
-;; Copyright (C) 2012 Barry Schwartz
+;; Copyright (C) 2012 Khaled Hosny and Barry Schwartz
+;; This file is part of the Sorts Mill Tools.
 ;; 
-;; This program is free software; you can redistribute it and/or modify
+;; Sorts Mill Tools is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3 of the License, or
 ;; (at your option) any later version.
 ;; 
-;; This program is distributed in the hope that it will be useful,
+;; Sorts Mill Tools is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
--- guile/sortsmill/kwargs.scm
+++ guile/sortsmill/kwargs.scm
@@ -2,21 +2,22 @@
 ;;
 ;; Adapted from (scheme kwargs), which is part of guile-lib.
 ;;
+;; Copyright (C) 2013 Khaled Hosny and Barry Schwartz
 ;; Copyright (C) 2003, 2004, 2007 Andy Wingo <wingo at pobox dot com>
-;; Copyright (C) 2013 Barry Schwartz
-
-;; This program is free software: you can redistribute it and/or modify
+;; This file is part of the Sorts Mill Tools.
+;; 
+;; Sorts Mill Tools is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
+;; the Free Software Foundation; either version 3 of the License, or
 ;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
+;; 
+;; Sorts Mill Tools is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+;; 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; 
