1b6302d9a322d5ed049df4554df4c2c06c2db654
Khaled Hosny <khaledhosny@eglug.org>
2013-05-26 16:25:00 +0200

    Rewrite in a more readable way

--- fontforge/fontview.c
+++ fontforge/fontview.c
@@ -3094,24 +3094,27 @@ VISIBLE void
 FVMenuSelectHintingNeeded (GWindow gw, struct gmenuitem *UNUSED (mi), GEvent *e)
 {
   FontView *fv = (FontView *) GDrawGetUserData (gw);
-  int i, gid, doit;
   EncMap *map = fv->b.map;
   SplineFont *sf = fv->b.sf;
-  int order2 = sf->layers[fv->b.active_layer].order2;
   int merge = SelMergeType (e);
 
-  for (i = 0; i < map->enc_limit; ++i)
+  for (int i = 0; i < map->enc_limit; ++i)
     {
-      doit = ((gid = enc_to_gid (map, i)) != -1 && sf->glyphs[gid] != NULL
-              && ((!order2 && sf->glyphs[gid]->changedsincelasthinted)
-                  || (order2
-                      && sf->glyphs[gid]->layers[fv->b.active_layer].splines !=
-                      NULL && sf->glyphs[gid]->ttf_instrs_len <= 0) || (order2
-                                                                        &&
-                                                                        sf->
-                                                                        glyphs
-                                                                        [gid]->
-                                                                        instructions_out_of_date)));
+      int doit = 0;
+      int gid = enc_to_gid (map, i);
+      if (gid != -1 && sf->glyphs[gid] != NULL)
+        {
+          if (sf->layers[fv->b.active_layer].order2)
+            {
+              if (sf->glyphs[gid]->layers[fv->b.active_layer].splines != NULL
+                  && sf->glyphs[gid]->ttf_instrs_len <= 0)
+                doit = 1;
+              else if (sf->glyphs[gid]->instructions_out_of_date)
+                doit = 1;
+            }
+          else if (sf->glyphs[gid]->changedsincelasthinted)
+            doit = 1;
+        }
       fv->b.selected[i] = mergefunc[merge + (fv->b.selected[i] ? 2 : 0) + doit];
     }
   GDrawRequestExpose (fv->v, NULL, false);
