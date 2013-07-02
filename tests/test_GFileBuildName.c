67540e911ceb5a1492c88fcc1bd38db8f6b0d602
Barry Schwartz <github@crudfactory.com>
2013-07-01 22:24:30 -0500

    test_GFileBuildName.c: Treat a missing argument as an empty string.

--- tests/test_GFileBuildName.c
+++ tests/test_GFileBuildName.c
@@ -10,8 +10,10 @@ main (int argc, char **argv)
 {
   GC_INIT ();
   setlocale (LC_ALL, "");
-  printf ("%s|", GFileBuildName (argv[1], argv[2]));
+  const char *arg1 = ((argc < 2) ? "" : argv[1]);
+  const char *arg2 = ((argc < 3) ? "" : argv[2]);
+  printf ("%s|", GFileBuildName (arg1, arg2));
   ulc_fprintf (stdout, "%U|",
-               u8_GFileBuildName ((uint8_t *) argv[1], (uint8_t *) argv[2]));
+               u8_GFileBuildName ((uint8_t *) arg1, (uint8_t *) arg2));
   return 0;
 }
