ecfc8274186db255c79c933115495ea0b53f5fdc
Barry Schwartz <github@crudfactory.com>
2013-07-01 22:51:47 -0500

    Get rid of a warning.

--- tests/test_GFileBuildName.c
+++ tests/test_GFileBuildName.c
@@ -1,5 +1,6 @@
 #include <config.h>
 #include <stdio.h>
+#include <string.h>
 #include <gc.h>
 #include <locale.h>
 #include <gfile.h>
@@ -10,8 +11,8 @@ main (int argc, char **argv)
 {
   GC_INIT ();
   setlocale (LC_ALL, "");
-  const char *arg1 = ((argc < 2) ? "" : argv[1]);
-  const char *arg2 = ((argc < 3) ? "" : argv[2]);
+  char *arg1 = ((argc < 2) ? strdup ("") : argv[1]);
+  char *arg2 = ((argc < 3) ? strdup ("") : argv[2]);
   printf ("%s|", GFileBuildName (arg1, arg2));
   ulc_fprintf (stdout, "%U|",
                u8_GFileBuildName ((uint8_t *) arg1, (uint8_t *) arg2));
