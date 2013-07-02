14496d127e1a867c733272168786cd1b5bb4524c
Barry Schwartz <github@crudfactory.com>
2013-07-01 22:22:14 -0500

    test_GFileBaseName.c: Treat a missing argument as an empty string.

--- tests/test_GFileBaseName.c
+++ tests/test_GFileBaseName.c
@@ -11,9 +11,10 @@ main (int argc, char **argv)
 {
   GC_INIT ();
   setlocale (LC_ALL, "");
-  printf ("%s|", GFileBaseName (argv[1]));
-  ulc_fprintf (stdout, "%U|", u8_GFileBaseName ((uint8_t *) argv[1]));
+  const char *arg1 = ((argc < 2) ? "" : argv[1]);
+  printf ("%s|", GFileBaseName (arg1));
+  ulc_fprintf (stdout, "%U|", u8_GFileBaseName ((uint8_t *) arg1));
   ulc_fprintf (stdout, "%llU|",
-               u32_GFileBaseName (x_gc_u8_to_u32 ((uint8_t *) argv[1])));
+               u32_GFileBaseName (x_gc_u8_to_u32 ((uint8_t *) arg1)));
   return 0;
 }
