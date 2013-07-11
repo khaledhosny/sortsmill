c90270a63be6cc9d3c1b3db8299a861618c33704
Barry Schwartz <github@crudfactory.com>
2013-07-11 14:20:28 -0500

    More overlooked import fixes.

--- python/sortsmill/cap_spacing.py
+++ python/sortsmill/cap_spacing.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-import fontforge
+import sortsmill.ffcompat as fontforge
 
 def cap_spacing(font, caps_names, expansion):
 
