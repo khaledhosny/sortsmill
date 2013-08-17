f8d75a86c1404d84c920fa9e43abb94944a6f5d7
Barry Schwartz <sortsmill@crudfactory.com>
2013-08-16 23:12:20 -0500

    Eliminate an extra layer of macro expansion (for no particular reason).

--- inc/sortsmill/initialized_global_constants.h
+++ inc/sortsmill/initialized_global_constants.h
@@ -29,7 +29,7 @@
 
 void scm_dynwind_pthread_mutex_unlock (void *mutex_ptr);
 
-#if STM_AT_LEAST_C99       /* For standard variadic macros support. */
+#if STM_AT_LEAST_C99            /* For standard variadic macros support. */
 
 /*
  * We use "double-checked locking".
@@ -38,14 +38,6 @@ void scm_dynwind_pthread_mutex_unlock (void *mutex_ptr);
  * Values defined with this macro are ‘lazy’; that is, they are not
  * initialized until the first time they are used.
  */
-
-#define __INITIALIZED_CONSTANT_THREAD_LOCK(MUTEX)       \
-  scm_dynwind_begin (0);                                \
-  pthread_mutex_lock (&MUTEX);                          \
-  scm_dynwind_pthread_mutex_unlock (&MUTEX)
-
-#define __INITIALIZED_CONSTANT_THREAD_UNLOCK(MUTEX)     \
-  scm_dynwind_end ()
 
 /* Unfortunately, in a C99-conforming program, the
    INITIALIZER_FUNCTION must take at least one extra argument, even if
@@ -56,9 +48,13 @@ void scm_dynwind_pthread_mutex_unlock (void *mutex_ptr);
   pthread_mutex_t __##NAME##__mutex = PTHREAD_MUTEX_INITIALIZER;        \
                                                                         \
   STM_ONE_TIME_INITIALIZE(MODIFIERS, TYPE, NAME,                        \
-                          __INITIALIZED_CONSTANT_THREAD_LOCK(__##NAME##__mutex), \
-                          __INITIALIZED_CONSTANT_THREAD_UNLOCK(__##NAME##__mutex), \
-                          INITIALIZER_FUNCTION (&NAME##__Value, __VA_ARGS__))
+                          scm_dynwind_begin (0);                        \
+                          pthread_mutex_lock (&__##NAME##__mutex);      \
+                          scm_dynwind_pthread_mutex_unlock              \
+                          (&__##NAME##__mutex),                         \
+                          scm_dynwind_end (),                           \
+                          INITIALIZER_FUNCTION (&NAME##__Value,         \
+                                                __VA_ARGS__))
 
 #endif /* STM_AT_LEAST_C99 */
 
