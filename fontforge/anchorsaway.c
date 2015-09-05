43b107c08b7dafd23456e5d00e0697660f031be8
Khaled Hosny <khaledhosny@eglug.org>
2015-09-05 10:27:56 +0200

    Revert "Fix crash when closing anchor dialog"
    This reverts commit e70461a8ec1cf8c754d5695fb84de294e297f80f.

--- fontforge/anchorsaway.c
+++ fontforge/anchorsaway.c
@@ -895,7 +895,7 @@ SetAnchor (SplineChar *sc, int layer, AnchorPoint *ap, DeviceTable *xadjust,
 {
   int ly;
 
-  if (ap->xadjust.corrections != NULL)
+  if (&ap->xadjust != NULL)
     free (ap->xadjust.corrections);
   if (xadjust->corrections == NULL)
     {
@@ -907,7 +907,7 @@ SetAnchor (SplineChar *sc, int layer, AnchorPoint *ap, DeviceTable *xadjust,
       xadjust->corrections = NULL;
     }
 
-  if (ap->yadjust.corrections != NULL)
+  if (&ap->yadjust != NULL)
     free (ap->yadjust.corrections);
   if (yadjust->corrections == NULL)
     {
